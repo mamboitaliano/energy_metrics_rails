7dfc019cb07e80b07a98d64fb4af4ca352e3b662
raidersofthenight <ianemailaddress@gmail.com>
2015-09-24 20:39:01 -0700

    create controller, create 'appliances/new' route, create basic views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  resources :appliances
+  
+  root 'home#index'
+end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,4 +58,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
